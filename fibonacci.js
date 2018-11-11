1c45c4384b93d039f4c986f04124158e751c548f
chemalopezp <chema.lopezp@gmail.com>
2018-11-11 12:05:40 -0800

    fibonacci algorithm

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+const fib = (n) => {
+    if (n==0){
+        return 0;
+    } else if (n==1) {
+        return 1;
+    } else {
+        return ((n-1) + fib(n-2));
+    }
+}
+
+console.log(fib(4));
